a5f9a614ad904cbe8ea640b153d113b2af63c6e0
Nedda Amini <Nedda.Amini@gmail.com>
2015-10-10 19:55:02 -0700

    webpack hot reloader is donnnnneeeee~

--- dev.server.js
+++ dev.server.js
@@ -1,12 +1,13 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var plugins = new webpack.HotModuleReplacementPlugin();
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(8080, 'localhost', function (err, result) {
   if (err) console.log(err);
   console.log('Listening at localhost:3000');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
- var path = require('path');
- var vis = require('vis');
+var path = require('path');
+var vis = require('vis');
+var webpack = require('webpack');
+var plugins = new webpack.HotModuleReplacementPlugin();
 
- module.exports = {
-   entry: './lib/app.js',
-   output: {
-     path: __dirname,
-     filename: 'bundle.js',
-     publicPath: '/bin'
-   },
-   module: {
-     loaders: [
-       { test: path.join(__dirname, 'lib'),
-         loader: 'babel-loader' }
-     ]
-   }
- };
+module.exports = {
+  entry: ['webpack/hot/dev-server',
+    './lib/app.js'],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/bin'
+  },
+  module: {
+    loaders: [
+      { test: path.join(__dirname, 'lib'),
+        loader: 'babel-loader' }
+    ]
+  }
+};
