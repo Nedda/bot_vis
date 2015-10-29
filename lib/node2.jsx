8036385b6e8a124f59256fa554ff77ecaba23f7c
Nedda Amini <Nedda.Amini@gmail.com>
2015-10-28 21:37:49 -0700

    SUPER SIMPLE COPY-PASTA TO SEE IF TECH WOULD PLAY WELL, it did :3

--- lib/node2.jsx
+++ lib/node2.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var d3 = require('d3');
+var d3Funcs = require('./d3charting.js');
+
+
+var processData = d3Funcs.processData;
+
+console.log(processData);
 
 var ComponentNode = React.createClass({
   render: function(){
@@ -8,6 +13,7 @@ var ComponentNode = React.createClass({
       <div>
         <div>bot vis</div>
         <div>hello world</div>
+        {d3Funcs.draw()}
       </div>
     );
   }
